2139039073605a641ef5251c6e7194fce4362298
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-01-03 20:47:39 +0100

    split command to separate module

---
+++ src/commands/export.rs
@@ -0,0 +1,79 @@
+use crate::CliArgs;
+use std::error::Error;
+use std::{collections::HashSet, fs, process};
+use crate::json_parse::parse_material_json;
+use crate::get_colors_from_meta::{get_colors_from_meta, MaterialProperty};
+use crate::get_filename::get_filename_for_material_property;
+use crate::save_image::{save_image, SaveImageSuccess};
+
+#[derive(Hash, PartialEq, Eq, Debug)]
+enum QueueOperation {
+    Export(MaterialProperty),
+}
+
+type QueueExport = HashSet<QueueOperation>;
+
+pub fn run(args: CliArgs) -> Result<(), Box<dyn Error>> {
+    match fs::read_to_string(&args.file) {
+        Ok(content) => {
+            if let Ok(meta) = parse_material_json(&content) {
+                let mut queue: QueueExport = HashSet::new();
+                if args.color {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Color));
+                }
+                if args.company_tint {
+                    queue.insert(QueueOperation::Export(MaterialProperty::CompanyTint));
+                }
+                if args.emission {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Emission));
+                }
+                if args.glassiness {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Glassiness));
+                }
+                if args.smoothness {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Smoothness));
+                }
+                if args.specular {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Specular));
+                }
+                if args.all {
+                    queue.insert(QueueOperation::Export(MaterialProperty::Color));
+                    queue.insert(QueueOperation::Export(MaterialProperty::CompanyTint));
+                    queue.insert(QueueOperation::Export(MaterialProperty::Emission));
+                    queue.insert(QueueOperation::Export(MaterialProperty::Glassiness));
+                    queue.insert(QueueOperation::Export(MaterialProperty::Smoothness));
+                    queue.insert(QueueOperation::Export(MaterialProperty::Specular));
+                }
+
+                if queue.len() == 0 {
+                    println!("Specify export operation. Use -h for help");
+                    process::exit(2);
+                }
+
+                for operation in queue {
+                    let QueueOperation::Export(material_type) = operation;
+                    let colors = get_colors_from_meta(&meta, &material_type);
+                    match save_image(
+                        &colors,
+                        &args.output.clone().into(),
+                        &get_filename_for_material_property(&material_type),
+                    ) {
+                        Ok(SaveImageSuccess::SaveOk(message)) => {
+                            if args.verbose {
+                                println!("{}", message);
+                            }
+                        }
+                        Err(e) => println!("Saving failed {:?}", e),
+                    }
+                }
+            } else {
+                println!("Invalid input file. Verify if you provided .meta file");
+                process::exit(1);
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to read the file: {}", e);
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod commands;
+
 mod get_colors_from_meta;
 mod get_filename;
 mod hex_to_rgb;
@@ -5,15 +7,11 @@ mod json_parse;
 mod save_image;
 
 use clap::Parser;
-use get_colors_from_meta::{get_colors_from_meta, MaterialProperty};
-use get_filename::get_filename_for_material_property;
-use json_parse::parse_material_json;
-use save_image::{save_image, SaveImageSuccess};
-use std::{collections::HashSet, fs, path::PathBuf, process};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
+pub struct CliArgs {
     file: PathBuf,
 
     #[arg(default_value_t = String::from("."))]
@@ -44,75 +42,8 @@ struct Args {
     all: bool,
 }
 
-#[derive(Hash, PartialEq, Eq, Debug)]
-enum QueueOperation {
-    Export(MaterialProperty),
-}
-
-type QueueExport = HashSet<QueueOperation>;
-
 fn main() {
-    let args = Args::parse();
+    let args = CliArgs::parse();
 
-    match fs::read_to_string(&args.file) {
-        Ok(content) => {
-            if let Ok(meta) = parse_material_json(&content) {
-                let mut queue: QueueExport = HashSet::new();
-                if args.color {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Color));
-                }
-                if args.company_tint {
-                    queue.insert(QueueOperation::Export(MaterialProperty::CompanyTint));
-                }
-                if args.emission {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Emission));
-                }
-                if args.glassiness {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Glassiness));
-                }
-                if args.smoothness {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Smoothness));
-                }
-                if args.specular {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Specular));
-                }
-                if args.all {
-                    queue.insert(QueueOperation::Export(MaterialProperty::Color));
-                    queue.insert(QueueOperation::Export(MaterialProperty::CompanyTint));
-                    queue.insert(QueueOperation::Export(MaterialProperty::Emission));
-                    queue.insert(QueueOperation::Export(MaterialProperty::Glassiness));
-                    queue.insert(QueueOperation::Export(MaterialProperty::Smoothness));
-                    queue.insert(QueueOperation::Export(MaterialProperty::Specular));
-                }
-
-                if queue.len() == 0 {
-                    println!("Specify export operation. Use -h for help");
-                    process::exit(2);
-                }
-
-                for operation in queue {
-                    let QueueOperation::Export(material_type) = operation;
-                    let colors = get_colors_from_meta(&meta, &material_type);
-                    match save_image(
-                        &colors,
-                        &args.output.clone().into(),
-                        &get_filename_for_material_property(&material_type),
-                    ) {
-                        Ok(SaveImageSuccess::SaveOk(message)) => {
-                            if args.verbose {
-                                println!("{}", message);
-                            }
-                        }
-                        Err(e) => println!("Saving failed {:?}", e),
-                    }
-                }
-            } else {
-                println!("Invalid input file. Verify if you provided .meta file");
-                process::exit(1);
-            }
-        }
-        Err(e) => {
-            eprintln!("Failed to read the file: {}", e);
-        }
-    }
+    commands::export::run(args).expect("error");
 }
