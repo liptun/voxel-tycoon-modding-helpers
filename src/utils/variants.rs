baa6a915bba6cc66a15dcf5d27d8055cb4e642c9
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-04-07 09:44:44 +0200

    update schema and get all nested variants names

---
+++ src/utils/variants.rs
@@ -0,0 +1,150 @@
+use super::json_parse::{VTMetaSchema, VariantSchema};
+
+fn get_variants_names_from_variant(variant: &VariantSchema) -> Option<Vec<String>> {
+    let mut variant_names: Vec<String> = Vec::new();
+
+    if let Some(variants) = &variant.variants {
+        for (key, variant) in variants {
+            variant_names.push(key.to_owned());
+            let nested_names = get_variants_names_from_variant(variant);
+            if let Some(nested_keys) = nested_names {
+                variant_names.extend(nested_keys);
+            }
+        }
+        return Some(variant_names);
+    }
+
+    None
+}
+
+pub fn get_nested_variants_names_from_meta(meta: &VTMetaSchema) -> Option<Vec<String>> {
+    let mut variant_names: Vec<String> = Vec::new();
+    if let Some(variants) = &meta.variants {
+        for (key, variant) in variants {
+            variant_names.push(key.to_owned());
+            let nested_names = get_variants_names_from_variant(variant);
+            if let Some(nested_keys) = nested_names {
+                variant_names.extend(nested_keys);
+            }
+        }
+        if !variant_names.is_empty() {
+            return Some(variant_names);
+        }
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::json_parse::parse_material_json;
+
+    use super::*;
+
+    fn get_test_data_without_variants() -> String {
+        r#"
+        {
+          "Materials": [
+            {
+              "Color": "424242",
+              "CompanyTint": 21,
+              "Emission": 37,
+              "Glassiness": 69,
+              "Smoothness": 66,
+              "Specular": 13
+            }
+          ],
+          "Variants": null
+        }"#
+        .to_string()
+    }
+
+    fn get_test_data_with_variants() -> String {
+        r#"{
+  "Materials": [
+    { "Color": "404040", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "9f1200", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "ffffff", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "404040", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "32a6a6", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "272727", "CompanyTint": 0, "Emission": 0, "Glassiness": 255, "Smoothness": 0, "Specular": 255 },
+    { "Color": "c6c6c6", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "f4dda4", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "404040", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "ffffff", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "ffffff", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "f4dda4", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "1ca14a", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "404040", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "c7171e", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+    { "Color": "858585", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+  ],
+  "Variants": {
+    "foo": {
+      "Materials": {
+        "12": { "Color": "a11c1c", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+      },
+      "Variants": {
+        "bar": {
+          "Materials": {
+            "15": { "Color": "bb4040", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+          },
+          "Variants": {
+            "lorem": {
+              "Materials": {
+                "7": { "Color": "505050", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+              },
+              "Variants": {
+                "ipsum": {
+                  "Materials": {
+                    "11": { "Color": "f0a4f4", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+                  },
+                  "Variants": {
+                    "deep": {
+                      "Materials": {
+                        "12": { "Color": "a18b1c", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 },
+                        "15": { "Color": "ce7837", "CompanyTint": 0, "Emission": 0, "Glassiness": 0, "Smoothness": 0, "Specular": 0 }
+                      },
+                      "Variants": null
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    "night": {
+      "Materials": {
+        "1": { "Color": "9f1200", "CompanyTint": 0, "Emission": 255, "Glassiness": 0, "Smoothness": 191, "Specular": 0 },
+        "5": { "Color": "d9ac4c", "CompanyTint": 0, "Emission": 255, "Glassiness": 255, "Smoothness": 160, "Specular": 255 }
+      },
+      "Variants": null
+    }
+  }
+}"#.to_string()
+    }
+
+    #[test]
+    fn test_get_variants_names() {
+        let meta = parse_material_json(&get_test_data_with_variants()).expect("Should create meta");
+        let mut variants = get_nested_variants_names_from_meta(&meta).expect("Should get variants");
+        variants.sort();
+
+        let mut expected_variants = vec![
+            "foo".to_string(),
+            "bar".to_string(),
+            "night".to_string(),
+            "lorem".to_string(),
+            "ipsum".to_string(),
+            "deep".to_string(),
+        ];
+        expected_variants.sort();
+        assert_eq!(variants, expected_variants);
+
+        let meta_without =
+            parse_material_json(&get_test_data_without_variants()).expect("Should create meta");
+        assert_eq!(get_nested_variants_names_from_meta(&meta_without), None);
+    }
+}
