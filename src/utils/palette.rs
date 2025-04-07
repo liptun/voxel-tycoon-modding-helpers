b56a965ec960ad9a3e98e044880be9e13492d63f
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2025-04-07 09:52:30 +0200

    cleanup

--- src/utils/palette.rs
+++ src/utils/palette.rs
@@ -1,33 +1,9 @@
-use super::json_parse::{MaterialSchema, VTMetaSchema, VariantSchema};
+use super::{
+    json_parse::{MaterialSchema, VTMetaSchema},
+    variants::get_variant_from_meta,
+};
 
 pub type VTPalette = Vec<MaterialSchema>;
-
-pub fn get_variants_names_from_meta(meta: &VTMetaSchema) -> Option<Vec<String>> {
-    let mut variant_names: Vec<String> = Vec::new();
-    if let Some(variants) = &meta.variants {
-        for (key, _) in variants {
-            variant_names.push(key.to_owned());
-        }
-        if !variant_names.is_empty() {
-            return Some(variant_names);
-        }
-    }
-
-    None
-}
-
-pub fn get_variant_from_meta<'a>(
-    meta: &'a VTMetaSchema,
-    variant: &'a String,
-) -> Option<&'a VariantSchema> {
-    if let Some(meta_variants) = &meta.variants {
-        if let Some(variant_colors) = &meta_variants.get(variant) {
-            return Some(variant_colors);
-        }
-    }
-
-    None
-}
 
 #[derive(Debug)]
 pub enum GetPaletteError {
@@ -59,24 +35,6 @@ mod tests {
     use crate::utils::json_parse::parse_material_json;
 
     use super::*;
-
-    fn get_test_data_without_variants() -> String {
-        r#"
-        {
-          "Materials": [
-            {
-              "Color": "424242",
-              "CompanyTint": 21,
-              "Emission": 37,
-              "Glassiness": 69,
-              "Smoothness": 66,
-              "Specular": 13
-            }
-          ],
-          "Variants": null
-        }"#
-        .to_string()
-    }
 
     fn get_test_data_with_variants() -> String {
         r#"
@@ -127,21 +85,6 @@ mod tests {
           }
         }"#
         .to_string()
-    }
-
-    #[test]
-    fn test_get_variants_names() {
-        let meta = parse_material_json(&get_test_data_with_variants()).expect("Should create meta");
-        let mut variants = get_variants_names_from_meta(&meta).expect("Should get variants");
-        variants.sort();
-
-        let mut expected_variants = vec!["Foo".to_string(), "Bar".to_string()];
-        expected_variants.sort();
-        assert_eq!(variants, expected_variants);
-
-        let meta_without =
-            parse_material_json(&get_test_data_without_variants()).expect("Should create meta");
-        assert_eq!(get_variants_names_from_meta(&meta_without), None);
     }
 
     #[test]
